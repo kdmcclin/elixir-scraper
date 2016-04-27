068ea1736a1c451f437d1e2e8ae6b2eeb6cdbffb
Katherine McClintic <katherine.mcclintic@livingsocial.com>
2016-04-27 19:18:12 -0400

    Add deps to mix config

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Scraper.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison, :hound]]
   end
 
   # Dependencies can be Hex packages:
